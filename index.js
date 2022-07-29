8e07f9f2475652269c59a8beb2e950dd16c97d59
Molka-jazi <molka.eljazi@esprit.tn>
2022-07-29 14:32:57 +0100

    final with correction

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express")
-
+const cors=require('cors')
 const app = express();
 
 app.use(express.json())
-
+app.use(cors())
 app.use('/auth', require("./routes/auth"))
 
 app.use('/crud',require("./routes/crud"))
